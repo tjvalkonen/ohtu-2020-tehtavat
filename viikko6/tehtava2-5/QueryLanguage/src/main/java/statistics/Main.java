126368fd6299c74952552e7517477a945da0648c
Tomas Valkonen <tomas.valkonen@gmail.com>
2020-12-04 00:11:40 +0200

    Tehtävä 3.

--- viikko6/tehtava2-5/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/tehtava2-5/QueryLanguage/src/main/java/statistics/Main.java
@@ -11,28 +11,45 @@ public class Main {
         //Matcher m = new And( new HasAtLeast(5, "goals"),
         //                     new HasAtLeast(5, "assists"),
         //                     new PlaysIn("PHI")
-        Matcher m = new And( 
+        System.out.println("---M1---");
+        Matcher m1 = new And( 
                                new HasFewerThan(1, "goals"), 
                                new PlaysIn("NYR")
         );
         
-        for (Player player : stats.matches(m)) {
+        for (Player player : stats.matches(m1)) {
             System.out.println(player);
         }
 
 
         System.out.println(stats.matches(new All()).size());
 
-
-        Matcher mm = new And( 
+        System.out.println("---M2---");
+        Matcher m2 = new And( 
                                new Not( new HasAtLeast(1, "goals") ), 
                                new PlaysIn("NYR")
         );
-        for (Player player : stats.matches(mm)) {
+        for (Player player : stats.matches(m2)) {
             System.out.println(player);
         }
+        System.out.println("---M3---");
+        Matcher m3 = new Or(new HasAtLeast(40, "goals"),
+                               new HasAtLeast(60, "assists")
+        );  
 
-
-
+        for (Player player : stats.matches(m3)) {
+            System.out.println(player);
+        }
+        System.out.println("---M4---");
+        Matcher m4 = new And(new HasAtLeast(50, "points"),
+                         new Or( 
+                         new PlaysIn("NYR"),
+                         new PlaysIn("NYI"),
+                         new PlaysIn("BOS")
+                            )
+                       );
+        for (Player player : stats.matches(m4)) {
+            System.out.println(player);
+        }
     }
 }
